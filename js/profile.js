ccbadfd662ad69b6858de690d0671aa105620c4b
MiguelD-PC <mikdominguez@gmail.com>
2022-04-25 15:06:25 +0200

    game page

--- js/profile.js
+++ js/profile.js
@@ -1,39 +1,32 @@
 let button = document.querySelector("#contBtn");
 let userObj = {
-    userName:"",
-    userAge:"",
-    userRecord:""
+    userName: "",
+    userAge: "",
+    userRecord: ""
 }
 
-button.addEventListener("click", function dataStore()
-{
-        let name = document.getElementById("userName").value;
-        let age = document.getElementById("userAge").value;
-        userObj.userName = name;
-        userObj.userAge= age;
-        localStorage.setItem(name , JSON.stringify(userObj));
-
-         ;
-        
-    // });
-
+button.addEventListener("click", function dataStore() {
+    let name = document.getElementById("userName").value;
+    let age = document.getElementById("userAge").value;
+    userObj.userName = name;
+    userObj.userAge = age;
+    localStorage.setItem(name, JSON.stringify(userObj));
 })
-button.addEventListener("click",  show)
+
+button.addEventListener("click", show)
+
 function show() {
     document.querySelector(".display-game").style.display = "block";
     document.querySelector(".profile").style.display = "none";
 }
 
-// addEventListener.button("click", )
-// function 
+document.querySelector("#gameStart").addEventListener("click", randomTimer)
 
-document.querySelector("#gameStart").addEventListener("click", randomTimer )
-  
-function randomTimer (){
-    let randomSeconds = Math.random()*10000;
-    let intervalSeconds = setTimeout(function (){
+function randomTimer() {
+    let randomSeconds = Math.random() * 10000;
+    let intervalSeconds = setTimeout(function () {
         const gameStop = document.getElementById("gameStop");
         gameStop.style.display = "block";
         console.log("hello");
-        }
-    ,randomSeconds);}
+    }, randomSeconds);
+}
