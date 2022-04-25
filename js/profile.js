2b72f320ed9e4c1ab972a285211eaa4e6406c626
Joan Gau Gonell <joangau18@hotmail.com>
2022-04-25 18:19:38 +0200

    timer Score

--- js/profile.js
+++ js/profile.js
@@ -1,27 +1,29 @@
 
+const playerName="";
+
 //RANKING Y DATA STORAGE
 let button = document.querySelector("#contBtn");
 let userObj = {
-    userName:"",
-    userAge:"",
-    userRecord:"",
+    userName: "",
+    userAge: "",
+    userRecord: "",
 }
 
-function dataStore()
-{
-        let name = document.getElementById("userName").value;
-        let age = document.getElementById("userAge").value;
-        userObj.userName = name;
-        userObj.userAge= age;
-        localStorage.setItem(name , JSON.stringify(userObj));
+function dataStore() {
+    let name = document.getElementById("userName").value;
+    let age = document.getElementById("userAge").value;
+    userObj.userName = name;
+    playerName= name;
+    userObj.userAge = age;
+    localStorage.setItem(name, JSON.stringify(userObj));
 
-         ;}
+    ;
+}
 function show() {
     document.querySelector(".display-game").style.display = "block";
     document.querySelector(".profile").style.display = "none";
 }
-function multi()
-{ 
+function multi() {
     dataStore();
     show();
 };
@@ -34,12 +36,36 @@ button.addEventListener("click", multi);
 
 
 //TIMER
-document.querySelector("#gameStart").addEventListener("click", randomTimer )
-  function randomTimer (){
-    let randomSeconds = Math.random()*10000;
-    let intervalSeconds = setTimeout(function (){
-        const gameStop = document.getElementById("gameStop");
-        gameStop.style.display = "block";
-        console.log("hello");
+let secondsElapsed = 0;
+let milisecElapsed = 0;
+
+const buttonStart = document.querySelector("#gameStart");
+buttonStart.addEventListener("click", randomTimer)
+
+function randomTimer() {
+    buttonStart.style.display = "none";
+    let randomSeconds = Math.random() * 10000;
+    let gameStop = document.querySelector(".button-end");
+    let timeOutSeconds = setTimeout(function () {
+        gameStop.style.display = "flex";
+        function timeElapsed() {
+            milisecElapsed++;
+                            }
+        
+
+        let milisecondsElapsed = setInterval(timeElapsed, 1000)
+        let buttonStop = document.querySelector("#gameEnd");
+        buttonStop.addEventListener("click", printTime)
+
+        function printTime() {
+            clearInterval(milisecondsElapsed);
+            console.log(milisecElapsed);
+            // userObj.userRecord=milisecElapsed;
+            JSON.parse(localStorage.getItem(playerName)).userRecord = milisecElapsed  ;
+
+            // console.log(milisecElapsed);
+            // console.log(secondsElapsed)
         }
-    ,randomSeconds);}
+
+    }, randomSeconds);
+}
