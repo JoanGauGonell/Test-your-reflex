3598e4898bcbc2821b82ccc07f1bfbe2600a405d
Joan Gau Gonell <joangau18@hotmail.com>
2022-04-25 14:51:10 +0200

    button next

--- js/profile.js
+++ js/profile.js
@@ -1,11 +1,13 @@
+
+//RANKING Y DATA STORAGE
 let button = document.querySelector("#contBtn");
 let userObj = {
     userName:"",
     userAge:"",
-    userRecord:""
+    userRecord:"",
 }
 
-button.addEventListener("click", function dataStore()
+function dataStore()
 {
         let name = document.getElementById("userName").value;
         let age = document.getElementById("userAge").value;
@@ -13,23 +15,27 @@ button.addEventListener("click", function dataStore()
         userObj.userAge= age;
         localStorage.setItem(name , JSON.stringify(userObj));
 
-         ;
-        
-    // });
-
-})
-button.addEventListener("click",  show)
+         ;}
 function show() {
     document.querySelector(".display-game").style.display = "block";
     document.querySelector(".profile").style.display = "none";
 }
+function multi()
+{ 
+    dataStore();
+    show();
+};
+button.addEventListener("click", multi);
+// function iterateLocalSt (){
+//      function(){
 
-// addEventListener.button("click", )
-// function 
+//     }
+// }
 
+
+//TIMER
 document.querySelector("#gameStart").addEventListener("click", randomTimer )
-  
-function randomTimer (){
+  function randomTimer (){
     let randomSeconds = Math.random()*10000;
     let intervalSeconds = setTimeout(function (){
         const gameStop = document.getElementById("gameStop");
