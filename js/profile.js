69852d2cb86d7f3ef58cd428c5d7ced3b0d8e1a7
Joan Gau Gonell <joangau18@hotmail.com>
2022-04-25 09:31:45 +0200

    update

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,18 @@
-document.querySelector("#gameStart").addEventListener("click", tenSecondsTimer )
-function tenSecondsTimer(){
+
+
+
+
+
+
+document.querySelector("#gameStart").addEventListener("click", randomTimer )
+  
+function randomTimer (){
+    let randomSeconds = Math.random()*10000;
+    let intervalSeconds = setTimeout(function (){
+        const gameStop = document.getElementById("gameStop");
+        gameStop.style.display = "block";
+        console.log("hello");
+        }
+    ,randomSeconds);
 
 }
